fa6e2bcd6bccd53dcc4b50c0eeaeb4df2fa7956d
unknown <insigne11@gmail.com>
2021-07-16 19:13:42 +0200

    delete

--- components/context.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const AuthContext = React.createContext();
-export const DataContext = React.createContext();
-export const CommandContext = React.createContext();
-export const ShowDataOpen = React.createContext();
-export const DataStatusContext = React.createContext();
-
-
