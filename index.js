ebf16ba456a8c77f3e1629b65aabefc72912c623
unknown <insigne11@gmail.com>
2021-07-17 16:50:14 +0200

    delete all

--- index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import Setup from './Setup';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => Setup);
